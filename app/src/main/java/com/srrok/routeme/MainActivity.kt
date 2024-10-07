1672c849362310cafe45db716663f0328f885034
Sыrok <alexandersavunov@yandex.ru>
2024-10-07 12:04:49 +0700

    Automatic commit

--- app/src/main/java/com/srrok/routeme/MainActivity.kt
+++ app/src/main/java/com/srrok/routeme/MainActivity.kt
@@ -3,35 +3,49 @@ package com.srrok.routeme
 
 //Библиотека
 import android.os.Bundle
+import android.os.Vibrator
 import android.app.Activity
 import android.webkit.WebView
+import android.app.AlertDialog
+import android.content.Context
 import android.webkit.WebSettings
-import androidx.core.view.ViewCompat
 import android.view.ViewTreeObserver
+import androidx.core.view.ViewCompat
 import android.annotation.SuppressLint
 import androidx.core.view.WindowInsetsCompat
 import androidx.constraintlayout.widget.ConstraintLayout
 
 //Базовый экран
-class MainActivity : Activity() {
+@Suppress("DEPRECATION")
+class MainActivity: Activity() {
+  //Компоненты WebView
+  private lateinit var webView: WebView
+  private lateinit var webSettings: WebSettings
+
+  //Сервис вибратора
+  private lateinit var vibrator: Vibrator
+  private var canVibrate: Boolean = false
+
   //При создании
   @SuppressLint("SetJavaScriptEnabled")
   override fun onCreate(savedInstanceState: Bundle?) {
     //Инициализация экрана и родителя
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    //Инициализация вибратора
+    vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    canVibrate = vibrator.hasVibrator()
     //Получаем WebView и параметры по ID
-    val webView = findViewById<WebView>(R.id.view)
-    val webSettings = webView.settings
+    webView = findViewById(R.id.view)
+    webSettings = webView.settings
     //Присваиваем ссылку
     webView.loadUrl(getString(R.string.link))
     //Параметры кэширования страницы
-    webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
+    webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
     //Параметры JavaScript, геолокации и хранилища
     webSettings.javaScriptEnabled = true
     webSettings.domStorageEnabled = true
     webSettings.setGeolocationEnabled(true)
-    webSettings.javaScriptCanOpenWindowsAutomatically = true
     webSettings.javaScriptCanOpenWindowsAutomatically = true
     //Отключаем масштабирование
     webSettings.setSupportZoom(false)
@@ -42,7 +56,7 @@ class MainActivity : Activity() {
     //Получаем ConstraintLayout по ID
     val mainConstraintLayout = findViewById<ConstraintLayout>(R.id.main)
     //Устанавливаем слушатель для получения отступов
-    mainConstraintLayout.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+    mainConstraintLayout.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener {
       //При инициализации экрана
       override fun onGlobalLayout() {
         //Удаляем слушателя после получения отступов
@@ -50,10 +64,35 @@ class MainActivity : Activity() {
         //Устанавливаем слушатель для отступов
         ViewCompat.setOnApplyWindowInsetsListener(mainConstraintLayout) { v, insets ->
           val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+          // Установка отступов:
           v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
           insets
         }
       }
     })
+  }
+
+  //Функция обратного перехода
+  @Deprecated("Deprecated in Java")
+  override fun onBackPressed() {
+    //Обработчик диалога
+    val builder = AlertDialog.Builder(this)
+    //Если есть возможность вернуться
+    if (webView.canGoBack()) {
+      //Возвращаемся
+      webView.goBack()
+    } else {
+      //Если может вибрировать
+      if (canVibrate) {
+        //Вибрируем
+        vibrator.vibrate(100L)
+      }
+      //Создаем диалог
+      builder.setTitle("Предупреждение")
+      builder.setMessage("Хотите выйти?")
+      builder.setPositiveButton("Остаться") { _, _ -> }
+      builder.setNegativeButton("Выйти") { _, _ -> finish() }
+      builder.show()
+    }
   }
 }
